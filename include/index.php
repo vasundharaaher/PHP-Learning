07697897dffc8899a1f5af9f3af9d2e0849646f0
vasundharaaher <122102502+vasundharaaher@users.noreply.github.com>
2025-09-30 09:11:13 +0530

    Add files via upload

---
+++ include/index.php
@@ -0,0 +1,28 @@
+<?php 
+        include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the Home Page <br>
+    Stuff about your Home page can go here<br>
+
+</body>
+</html>
+
+
+<?php 
+        include("footer.html");
+?>
+
+<!-- 
+// include() :  Copies the content of a file (php/html/text)
+    //              and include it in your php file.
+    //              Section of our website become reusable
+    //              Changes only need to be made in one place -->
